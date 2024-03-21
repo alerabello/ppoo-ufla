844a9b84293369da68fee593451ecf42a30d973a
alexandre rabello <alerabello95@hotmail.com>
2024-03-21 20:08:47 -0300

    Add - Passo 6 - Evitando atributos e métodos estáticos

---
+++ Semana_2_Carro/src/Principal.java
@@ -0,0 +1,58 @@
+// Classe Principal
+
+import java.util.Scanner;
+
+public class Principal {
+    private Carro carro;
+    private Scanner entrada = new Scanner(System.in);
+
+    public void tratarOpcaoMenu(int opcao) {
+        switch (opcao) {
+            case 1:
+                System.out.println("Digite o modelo do carro:");
+                String modelo = entrada.nextLine();
+                carro = new Carro(modelo, 10);
+                break;
+            case 2:
+                if (carro != null) {
+                    carro.acelerar();
+                } else {
+                    System.out.println("Carro não criado.");
+                }
+                break;
+            case 3:
+                if (carro != null) {
+                    carro.reduzirVelocidade();
+                } else {
+                    System.out.println("Carro não criado.");
+                }
+                break;
+            case 4:
+                if (carro != null) {
+                    System.out.println("Modelo: " + carro.getModelo());
+                    System.out.println("Velocidade: " + carro.getVelocidade());
+                } else {
+                    System.out.println("Carro não criado.");
+                }
+                break;
+            case 5:
+                System.out.println("Saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida.");
+        }
+    }
+
+    public void executar() {
+        exibirMenu();
+    }
+
+    public void exibirMenu() {
+        System.out.println("1. Criar o carro");
+        System.out.println("2. Acelerar o carro");
+        System.out.println("3. Reduzir a velocidade");
+        System.out.println("4. Exibir os dados do carro");
+        System.out.println("5. Sair");
+        System.out.println("Digite sua opção:");
+    }
+}
